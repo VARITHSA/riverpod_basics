a2b124a6a59bb734a0a708ba1f000ced2846ce90
Srivathsa <bssrivathsa2003@gmail.com>
2023-02-17 12:18:19 +0530

    simple toggle app counter to understand riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// * Riverpod gives conviences to work with and also watch the changes on the value
-// * It is all about providing values asynchronously and watch those changes and make the different changes as necessary
-// * They are global function
-// * When providers are created with riverpod something like providerScope should also be mentioned since ProviderScope allows us handle diiferent providers using riverpods
-// * ProviderScope is the bridge b/w riverpod and Flutter world
-// ! We are doing 6 Examples each in different branches
-// ? we are also using flutter hooks also
-
+// * we are using stateNotifier and stateNotifierProvider
+// * we are going to havea a simple counter application and every time the button gets pressed the counter needs to update itself
+// ! This is Example -2 of riverpod_basics
+// * StateNotifier is the class from the flutter not from rivepod, we can set and read the value
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
+
+// ! we are creating Infix operator
+final stateProvider = StateProvider<int>(((ref) {
+  return 0;
+}));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -31,29 +32,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// ! creating 1st provider without any parameters but output is date and time
-// ? here is ref is a window to the applicaiton and can be used
-final currentDate = Provider<DateTime>(
-  (ref) => DateTime.now(),
-);
-
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // * ref actually changes it in real time
-    // ? wath() -> helps to watch the provider and asks to rebuild the widget whenever required
-    final date = ref.watch(currentDate);
+    final val = ref.watch(stateProvider);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Riverpod'),
+      appBar: AppBar(),
+      body: Center(child: Text('Value : $val')),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          FloatingActionButton(
+            onPressed: () => ref.read(stateProvider.notifier).state++,
+            child: const Center(child: Icon(Icons.add)),
+          ),
+          FloatingActionButton(
+            onPressed: () => ref.read(stateProvider.notifier).state--,
+            child: const Center(child: Icon(Icons.delete_outline)),
+          ),
+        ],
       ),
-      body: Center(
-          child: Text(
-        date.toIso8601String(),
-        style: Theme.of(context).textTheme.headline4,
-      )),
     );
   }
 }
